46e54a1960b30f0ec4f35d9b4a01b8305289d8d5
Pei-Yun Tseng <peiyun.t@gmail.com>
2020-08-14 16:48:16 -0500

    v1

---
+++ react-test/build/precache-manifest.0ce75918830bb436b5613ca79ae0eaa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3180ec21527e3ed69bc413c2d7b62a2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5040381868029c15f5e9",
+    "url": "/static/css/main.4b42239f.chunk.css"
+  },
+  {
+    "revision": "2150c77849fdd5922e59",
+    "url": "/static/js/2.35a3c364.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.35a3c364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5040381868029c15f5e9",
+    "url": "/static/js/main.f74d2397.chunk.js"
+  },
+  {
+    "revision": "1550bc5cb6224d84350f",
+    "url": "/static/js/runtime-main.74ac9606.js"
+  }
+]);
